92367e926ba40ee1bc8ef473362ff86c2e85a803
Nico Cortes <nico.crts@gmail.com>
2016-10-22 13:23:43 -0500

    swap now compiles. Going to merge this with my pageFaultHandler branch to complete that functionality. Then going to debug and test

--- vm/swap.c
+++ vm/swap.c
@@ -2,22 +2,25 @@
 #include <debug.h>
 #include <limits.h>
 #include <stdio.h>
-#include "swap.h"
-#include "../threads/vaddr.h"
+#include "devices/block.h"
+#include "userprog/syscall.h"
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "vm/swap.h"
 
-struct struct lock swap_lock;
+struct lock swap_lock;
 static uint32_t block_per_page;
 
 
 static struct block *swap_block;
-static struct block_sector_t swap_size;
+static block_sector_t swap_size;
 static struct bitmap* swap_bm;
 
 void swap_init(void){
   lock_init(&swap_lock);
   swap_block = block_get_role(BLOCK_SWAP);  
-  block_per_page = PGSIZE/swap_block->size;
-  swap_size = block_size(swap_block)/block_per_page;
+  block_per_page = PGSIZE / BLOCK_SECTOR_SIZE;
+  swap_size = block_size(swap_block) / block_per_page;
   /*bitmap to keep track of inuse swap slots; 0: not used*/
   swap_bm = bitmap_create(swap_size); 
 }
@@ -25,12 +28,13 @@ void swap_init(void){
 size_t swap_frame(void* page_ptr){
   lock_acquire(&swap_lock);
   /*search for empty slots*/ 
-  size_t swap_index = bitmap_scan_and_flip(swap_block,0,1,true);
+  size_t swap_index = bitmap_scan_and_flip(swap_bm, 0, 1, true);
 
 
   /*write page into swap*/
-  for (uint32_t i = 0; i < block_per_page; i++) {
-    block_write(swap_block, swap_index*block_per_page+i,(char*)page_ptr+swap_block->size);
+  uint32_t i;
+  for (i = 0; i < block_per_page; i++) {
+    block_write(swap_block, swap_index*block_per_page+i, page_ptr + BLOCK_SECTOR_SIZE * i);
   }
 
   lock_release(&swap_lock);
@@ -42,20 +46,21 @@ void restore_frame(void* page_ptr, size_t swap_index){
   lock_acquire(&swap_lock);
 
   /*read swap into page*/
-  for (uint32_t i = 0; i < block_per_page; i++) {
-    block_read(swap_block, swap_index*block_per_page+i,(char*)page_ptr+swap_block->size);
+  uint32_t i;
+  for (i = 0; i < block_per_page; i++) {
+    block_read(swap_block, swap_index*block_per_page+i, page_ptr + BLOCK_SECTOR_SIZE * i);
   }
 
   /*reclaim free swap slot*/
-  bitmap_set(swap_block,swap_index,false);
+  bitmap_set(swap_bm,swap_index,false);
   lock_release(&swap_lock);
 }
 
-void free_frame(size_t swap_index){
-  lock_acquire(&swap_lock);
-  if (swap_index > swap_size) exit(-1);
-  /*reclaim free swap slot*/
-  bitmap_set(swap_block,swap_index,false);
-  lock_release(&swap_lock);
-}
+/* void free_frame(size_t swap_index){ */
+/*   lock_acquire(&swap_lock); */
+/*   if (swap_index >= swap_size) exit(-1); */
+/*   /\*reclaim free swap slot*\/ */
+/*   bitmap_set(swap_bm,swap_index,false); */
+/*   lock_release(&swap_lock); */
+/* } */
 
--- vm/swap.h
+++ vm/swap.h
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 
 void swap_init(void);
 
